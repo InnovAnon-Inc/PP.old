b72770e34ab57f1048fe54f5c91dc81763ec43a9
Juju Gouldbergstein <gouldbergstein@durga.innovanon.com>
2018-09-15 04:57:28 -0500

    initial commit

---
+++ Simon/src/main/java/com/innovanon/rnd/simon/App.java
@@ -0,0 +1,13 @@
+package com.innovanon.rnd.simon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/innovanon/rnd/PP/App.java
@@ -0,0 +1,13 @@
+package com.innovanon.rnd.PP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
