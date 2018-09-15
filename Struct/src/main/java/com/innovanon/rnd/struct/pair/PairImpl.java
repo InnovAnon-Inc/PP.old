fca251c3314b710e287c0706850e96e4fcb16097
Juju Gouldbergstein <gouldbergstein@durga.innovanon.com>
2018-09-15 05:06:22 -0500

    added structs

---
+++ Struct/src/main/java/com/innovanon/rnd/struct/pair/PairImpl.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.innovanon.rnd.struct.pair;
+
+/**
+ * @author gouldbergstein
+ * @param <CAR> Contents of the Address part of Register number
+ * @param <CDR> Contents of the Decrement part of Register number
+ */
+public abstract class PairImpl<CAR, CDR> implements Pair<CAR, CDR> {
+	/**
+	 * @see #toString(Class)
+	 * @see Object#getClass()
+	 */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return toString(this.getClass());
+	}
+	
+	/**
+	 * @param subtype the type of the invoking object
+	 * @return a formated String for use with {@link #toString()}
+	 */
+	protected String toString(Class<?> subtype) {
+		return String.format("%s [car=%s, cdr=%s]", subtype.getSimpleName(),getCar(),getCdr());
+	}
+}
